6c1a50b122d3c5cfdf42b980b30265d7f4dbce90
guygubaby <guygubaby@gmail.com>
2021-06-24 17:40:15 +0800

    fix: remove some dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createPlugin, VueImageViewerPluginOptions } from "./index";
-import vhCheck from "vh-check";
 import "element-plus/lib/theme-chalk/index.css";
-
-vhCheck();
 
 const options: VueImageViewerPluginOptions = {
   directiveName: "viewer",
